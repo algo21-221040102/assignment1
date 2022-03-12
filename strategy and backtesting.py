6868cb0445422476192c95c7f9dcc63611526847
algo21-221040102 <221040102@link.cuhk.edu.cn>
2022-03-12 11:51:19 +0800

    Update strategy and backtesting.py

--- strategy and backtesting.py
+++ strategy and backtesting.py
@@ -40,11 +40,9 @@ data1.head()
 
 #%%
 data=data1.pct_change()
-# 计算beta序列
+# beta sequence
 # def Pearson(x,y):
-#     beta1=rets[['上证综指','贵州茅台']].cov().iat[0,1]/rets['上证综指'].var()
-
-#%%
+# beta1=rets[['上证综指','贵州茅台']].cov().iat[0,1]/rets['上证综指'].var()
 a=-1
 date=[]
 b=[]
@@ -59,6 +57,7 @@ for k in range(0,len(data.index),5):
 new_data
 
 #%%
+# calculate rate of return by week
 c=-1
 date1=[]
 d=[]
@@ -67,17 +66,18 @@ for j in range(0,len(data1.index),5):
 new_data_ret=pd.DataFrame(index=date1,columns=list(data1.columns)[:-1])
 print(len(data1.index))
 for k in range(0,len(data1.index),5):
-#     print(k)
+    #   print(k)
     c=c+1
     for i in list(data1.columns)[:-1]:
         if k<495:
-#         print(data1[i][k+5])
+    #   print(data1[i][k+5])
             new_data_ret[i][c]=data1[i][k+5]/data1[i][k]-1
         else:
             new_data_ret[i][c]=data1[i][k+4]/data1[i][k]-1
 new_data_ret
 
 #%%
+# calculate Spearman coefficient
 spear=[]
 for i in list(new_data.T.columns):   
     spear.append(new_data.T[i].corr(new_data_ret.T[i],method='spearman'))
@@ -87,9 +87,11 @@ spear_data['hs300']=data1['hs300']
 spear_data.head()
 
 #%%
+# Beta goes up with rate of return, then the emotion of investors turns optimistic
+# Beta goes down together with rate of return, then the emotion of investors turn pessimistic
 spear_data['short']=0
 spear_data['long']=0
-spear_data['spearman_corr']=spear_data['spearman_corr']*100
+spear_data['spearman_corr']=spear_data['spearman_corr']*100                        # 0.317*100=31.7
 for i in range(1,len(spear_data.index)):
     spear_data['short'][i]=np.where((spear_data['spearman_corr'][i-1]>31.7) and 
                                    (spear_data['spearman_corr'][i]<=31.7) and 
@@ -100,12 +102,13 @@ for i in range(1,len(spear_data.index)):
 spear_data
 
 #%%
+# show the long and short list between the testing period
 spear_data['str']=np.where(spear_data['short']>0,-1,np.where(spear_data['long']>0,1,0))
 spear_data.head()
 
 spear_data['px']=np.arange(len(spear_data))
 long=list(spear_data[spear_data['str']==1].index)
-short=['2018-11-21','2019-03-15','2019-07-19','2019-12-30','2020-05-12','2020-08-06']
+short=['2019-12-19','2020-02-10','2020-04-07','2020-05-08','2020-05-29','2020-07-07']
 short_n=[]
 long_n=[]
 for i in short:
@@ -122,7 +125,7 @@ spear_data
 for i in range(len(long)):
     date_long=long[i]   
     date_short=short[i]
-    spear_data['mak'][date_long, date_short]=1
+    #spear_data['mak'][date_long: date_short]=1
 spear_data['mak']=np.where(spear_data['str']==-1,0,spear_data['mak'])
 spear_data
 
